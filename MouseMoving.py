7e14781ef950605763e783fd9d282b6ed4def8e7
mohsen1299@gmail.com <mohsen1299@gmail.com>
2022-12-02 23:46:38 +0330

    add fun project to move mouse automatically

---
+++ MouseMoving.py
@@ -0,0 +1,11 @@
+# fun project from https://www.youtube.com/@b001
+
+import pyautogui as pag
+import random
+import time
+
+while True:
+    x = random.randint(600,700)
+    y = random.randint(200,600)
+    pag.moveTo(x,y,0.5)
+    time.sleep(2)
